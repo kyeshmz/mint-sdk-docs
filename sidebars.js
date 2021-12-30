7f5fb1e7b25af42cbc2521b65c8c99e567055d3c
tomonari-t <tomo1208.japan@gmail.com>
2021-12-30 12:09:53 +0900

    first commit

---
+++ sidebars.js
@@ -0,0 +1,17 @@
+module.exports = {
+  main: {
+    'Getting Started': [
+      'gettingStarted/Tutorial',
+      'gettingStarted/TutorialFixedPrice',
+      'gettingStarted/PhysicalItem',
+      'gettingStarted/UserAccountInfo',
+    ],
+    OpenSeaやEtherscanなどへのリンク生成方法: [
+      'tokenRelatedURL/openSea',
+      'tokenRelatedURL/blockchainExplorer',
+    ],
+    tokenIdについて: ['TokenIdDesign'],
+    通貨について: ['EthInSdk'],
+    'Mint SDK API': require('./typedoc-sidebar.js'),
+  },
+}
